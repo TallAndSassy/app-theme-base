b530c61a56ad7222c5aa47ccada81b271dc4e04d
jjrohrer <jj.bitbucket@rohrer.org>
2020-10-16 11:20:22 -0400

    colors

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -6,8 +6,8 @@
 @php
 $bg =  "bg-gray-50";
 $classes = ($isActive ?? false)
-            ? 'bg-gray-50 inline-flex items-center px-2 pt-1 border-b-2 border-indigo-400 font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'bg-gray-50 inline-flex items-center px-2 pt-1 border-b-2 border-transparent  font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'bg-gray-100 inline-flex items-center px-2 pt-1 border-b-2 border-indigo-400 font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+            : 'bg-gray-100 inline-flex items-center px-2 pt-1 border-b-2 border-transparent  font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
