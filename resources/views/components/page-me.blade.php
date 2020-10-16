a38be84c6720a7550dd8285aa75066aff53d4afe
jjrohrer <jj.bitbucket@rohrer.org>
2020-10-16 11:00:23 -0400

    me works

--- resources/views/components/page-me.blade.php
+++ resources/views/components/page-me.blade.php
@@ -1,8 +1,22 @@
 <x-tassy::page-_base>
-    <div class="tassy::page-guide.components.page-me">
-    <x-tassy::header.back.index title="{{$title}}"/>
-    <div class="p-10 bg-gray-100">{{$slot}}</div>
+    {{--  Space out vertically so footer is always at bottom of screen, or below content, whichever is lower
+      see: https://stackoverflow.com/questions/59812003/make-footer-fixed-on-the-bottom-with-tailwindcss
+    --}}
+    <div class="flex flex-col h-screen justify-between bg-gray-100">
+        <x-tassy::header.back.index title="{{$title}}"/>
+        <main class="mb-auto  bg-gray-100">
+            <div class="p-10 h-full">{{$slot}}</div>
+        </main>
 
-    <x-tassy::footer.back.index/>
+        <x-tassy::footer.back.index/>
+
     </div>
+
+    {{--    <div class="tassy::page-guide.components.page-me bg-gray-100 ">--}}
+    {{--        <x-tassy::header.back.index title="{{$title}}"/>--}}
+
+    {{--        <div class="p-10 bg-gray-100 h-full">{{$slot}}</div>--}}
+
+    {{--        <x-tassy::footer.back.index/>--}}
+    {{--    </div>--}}
 </x-tassy::page-_base>
