5b58f05c527e06119d848062cb82c2d23a1dc52a
jjrohrer <jj.bitbucket@rohrer.org>
2020-10-16 11:24:52 -0400

    fix name linke bg color

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -4,10 +4,16 @@
 {{--    </x-jet-nav-link>--}}
 {{--     @props(['active'])--}}
 @php
-$bg =  "bg-gray-50";
+$isBack = (request()->routeIs('user*') || request()->routeIs('me*') || request()->routeIs('admin*') || request()->routeIs('teams*'));
+if ($isBack) {
+    $bgColor =  "bg-gray-100";
+} else {
+    $bgColor =  "bg-gray-50";
+}
+
 $classes = ($isActive ?? false)
-            ? 'bg-gray-100 inline-flex items-center px-2 pt-1 border-b-2 border-indigo-400 font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'bg-gray-100 inline-flex items-center px-2 pt-1 border-b-2 border-transparent  font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? $bgColor.'  inline-flex items-center px-2 pt-1 border-b-2 border-indigo-400 font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+            : $bgColor.' inline-flex items-center px-2 pt-1 border-b-2 border-transparent  font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
