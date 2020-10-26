bf2bf31c20238b7be578f29bf1387623cab06703
jjrohrer <jj.bitbucket@rohrer.org>
2020-10-26 08:19:50 -0400

    tweak footer

--- resources/views/components/footer/back/index.blade.php
+++ resources/views/components/footer/back/index.blade.php
@@ -1,3 +1,3 @@
-<div class="  flex flex-col sm:justify-center items-center py-6 bg-gray-100">
+<div class="  flex flex-col sm:justify-center items-center pb-1 pt-6 bg-gray-100">
     <x-tassy::footer.back.content/>
 </div>
