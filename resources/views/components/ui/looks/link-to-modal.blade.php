3360d6e22771a00416b1498b752893f205ed208a
jjrohrer <jj.bitbucket@rohrer.org>
2020-11-13 09:53:17 -0500

    tweak

--- resources/views/components/ui/looks/link-to-modal.blade.php
+++ resources/views/components/ui/looks/link-to-modal.blade.php
@@ -3,7 +3,7 @@
  @endphp
     <button
             href = "{{$href}}"
-            class = "text-sm font-medium text-indigo-600 "
+            class = "text-sm font-medium text-indigo-600 text-left"
             style = "border-bottom: 1px dotted; text-shadow: 0.5px 0.5px 1px #64748b"
             random_number = "{{$random_number}}"
             ><span>{!! $slot !!}</span>
